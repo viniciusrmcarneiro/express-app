8b5adf43f0269f2114e1d62e62ff736b8a8af4df
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2016-11-09 08:03:32 +1100

    user repo from json file

--- repo/user.js
+++ repo/user.js
@@ -1,10 +1,12 @@
-
 const fs = require('fs');
+const path = require('path');
+const fileName = path.join(__dirname,'./users.json');
 
 function getByUsername(username) {
 	return new Promise( resolve => {
-		const rawUsers = fs.readFileSync('./users.json');
-		console.log('á====',rawUsers);
+		const rawUsers = fs.readFileSync(fileName).toString();
+		const users = JSON.parse(rawUsers);
+		console.log({users})
 		resolve({username, password: '1234'});
 	});
 };
