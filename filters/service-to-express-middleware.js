f0dbff6a8b5ec93eef27502f99ee2c9727050b6a
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-01-03 08:01:31 +1100

    service wrapper test

--- filters/service-to-express-middleware.js
+++ filters/service-to-express-middleware.js
@@ -2,12 +2,14 @@ const logger = require('../utils/logger');
 
 const middleware = (ms, req, res) => {
     try {
+        console.log('1');
         const params = Object.assign({}, req.body, req.params, req.query);
         const context = {
             log: logger,
         };
 
         return new Promise( (resolve, rejec) => {
+        // console.log('2');
             ms(params, context)
                 .then(resolve)
                 .catch(rejec);
@@ -17,15 +19,17 @@ const middleware = (ms, req, res) => {
             res.send(data);
         })
         .catch( ex => {
-            console.log('\n',ex, '\n----------');
+            // console.log('\n',ex, '\n----------');
+            console.log('3 -> ', ex.httpCode || 500)
+            console.log('3 -> ', ex.message)
             res.status(ex.httpCode || 500);
             res.send(ex.message);
         });
 
     } catch (ex) {
-        console.log(ex.stack || ex);
-        req.status(500);
-        req.send(ex.message);
+        // console.log(ex.stack || ex);
+        res.status(500);
+        res.send(ex.message);
     }
 };
 
--- test/filters/test-service-to-express-middleware.js
+++ test/filters/test-service-to-express-middleware.js
@@ -13,16 +13,35 @@ describe('FILTERS', function(){
     describe('SERVICE TO MIDLLEWARE', function(){
         var target = require('../../filters/service-to-express-middleware');
         var sandbox = sinon.sandbox.create();
+        var req;
+        var res;
 
         beforeEach(function(){
+            req = mocks.req;
+            res = mocks.res;
+            
+            sandbox.spy(res, 'status');
+            sandbox.spy(res, 'send');
         });
 
         afterEach(function(){
             sandbox.restore();
         });
        
-        it('we should write some tests', function(){
-            
+        it.only('should return 500 if the function throws an error', function(){
+            const myError = new Error('some error');
+
+            // const msSpy = sandbox.stub();
+            // msSpy.throws(myError);
+        
+            const msSpy = sandbox.spy( () => {
+                throw myError;
+            });
+
+
+            target(() => msSpy)(req, res);
+            sinon.assert.calledWithExactly(res.status, 500);
+            sinon.assert.calledWithExactly(res.send, myError.message);
         });
     });
 
